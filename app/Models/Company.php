66ef8dcb2ec5a891e3f226905d5373eabc3ff167
Victor-Murani <victormurani@gmail.com>
2026-02-24 10:11:39 +0300

    Company model init

---
+++ app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanyFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * Get all of the customers for the Company
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function customers(): HasMany
+    {
+        return $this->hasMany(Customer::class);
+    }
+}
